b52bc53a4e88583e3d460d83c8d6e7a05a897b2b
AGD-2022 <117851653+AGD-2022@users.noreply.github.com>
2023-04-25 23:46:39 +0200

    Add files via upload

---
+++ checkpoint3.py
@@ -0,0 +1,36 @@
+# 1
+string = "hola"
+number = 1
+lista = [1,2,3]
+verdadero = True
+
+# 2
+tres_primeros = string[0:3]
+print(tres_primeros)
+
+# 3
+primero_lista = lista[0]
+print(primero_lista)
+
+# 4 
+plus_dicez = number + 10
+print(plus_dicez)
+
+# 5
+ultimo_lista = lista[-1]
+print(ultimo_lista)
+
+# 6
+names = 'harry,alex,susie,jared,gail,conner'
+cortados = names.split(",")
+print(f"{cortados} del ejercicio 6")
+
+# 7
+primero = cortados[0].upper()
+cortados[0] = primero
+print(f"{cortados} del ejercicio 7")
+# 8
+print(f"soy el numero {number}")
+
+# 9
+print("hello world")
